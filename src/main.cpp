65e2005a370318aa014b65a79e1ce03df51b34ae
daylyn-n <daylynnguyen33@gmail.com>
2026-01-28 12:48:27 -0800

    Some minor fix

--- src/main.cpp
+++ src/main.cpp
@@ -12,40 +12,54 @@ int main()
     
     //board test
     Board board;
+
+    sf::Text text;
+    sf::Font font;
     
-   board.set(0, 3, 2);
-   board.set(2, 3, 2);
-  
-    for(std::size_t i {}; i < 4; i++)
+    if(!font.loadFromFile("./assets/fonts/BitcountSingle-Regular.ttf"))
     {
-        for(std::size_t j {}; j < 4; j++)
-        {
-            std::cout << board.get(i, j) << ' ';
-        }
-        std::cout << '\n';
+	std::cout << "Font file not found" << std::endl;
     }
-    std::cout << '\n';
-    while(true)
-    {
-        char input;
-        std::cin >> input;
-        if(input == 'w') moves::apply(board, moves::Directions::UP);
-        else if(input == 'a')moves::apply(board, moves::Directions::LEFT);
-        else if(input == 'd')moves::apply(board, moves::Directions::RIGHT);
-        else moves::apply(board, moves::Directions::DOWN);
-        board.insertTwoOrFour();
-        for(std::size_t i {}; i < 4; i++)
-        {
-            for(std::size_t j {}; j < 4; j++)
-            {
-                std::cout << board.get(i, j) << ' ';
-            }
-            std::cout << '\n';
-        }
+    
+
+    text.setFont(font);
+    text.setCharacterSize(30);
+    text.setString("hellllvdvdxl");
+    text.setFillColor(sf::Color::Red);
+    
+//    board.set(0, 3, 2);
+//    board.set(2, 3, 2);
+  
+//     for(std::size_t i {}; i < 4; i++)
+//     {
+//         for(std::size_t j {}; j < 4; j++)
+//         {
+//             std::cout << board.get(i, j) << ' ';
+//         }
+//         std::cout << '\n';
+//     }
+//     std::cout << '\n';
+//     while(true)
+//     {
+//         char input;
+//         std::cin >> input;
+//         if(input == 'w') moves::apply(board, moves::Directions::UP);
+//         else if(input == 'a')moves::apply(board, moves::Directions::LEFT);
+//         else if(input == 'd')moves::apply(board, moves::Directions::RIGHT);
+//         else moves::apply(board, moves::Directions::DOWN);
+//         board.insertTwoOrFour();
+//         for(std::size_t i {}; i < 4; i++)
+//         {
+//             for(std::size_t j {}; j < 4; j++)
+//             {
+//                 std::cout << board.get(i, j) << ' ';
+//             }
+//             std::cout << '\n';
+//         }
        // board.insertTwoOrFour();
 
        
-    }
+    //}
    
 
         while(window.isOpen())
@@ -55,6 +69,9 @@ int main()
             {
                 if(e.type == sf::Event::Closed) window.close();
             }
+        window.clear();
+	    window.draw(text);
+        window.display();
     }
     return 0;
 }
